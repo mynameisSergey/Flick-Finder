14fa552d29b56fbbf4d96d520818f0212aec90ce
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-02-27 23:38:34 +0300

    fis: исправлена команда добавления данных в базу

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,15 @@
-MERGE INTO mpa_ratings (mpa_rating_id, name)
+INSERT INTO mpa_ratings (mpa_rating_id, name)
     VALUES (1, 'G'),
            (2, 'PG'),
            (3, 'PG-13'),
            (4, 'R'),
            (5, 'NC-17');
-
-MERGE INTO genres (genre_id, name)
+ON CONFLICT (mpa_rating_id) DO NOTHING;
+INSERT INTO genres (genre_id, name)
     VALUES (1, 'Комедия'),
            (2, 'Драма'),
            (3, 'Мультфильм'),
            (4, 'Триллер'),
            (5, 'Документальный'),
            (6, 'Боевик');
+ON CONFLICT (genre_id) DO NOTHING;
