23a82ed8bc1295a285ac2ead235a4c981cf9bdad
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-02-27 22:53:10 +0300

    fix: исправлена валидация данных

--- src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
@@ -2,10 +2,8 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exсeption.validate.IncorrectParamException;
 import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.service.MpaService;
 
@@ -20,11 +18,19 @@ public class MpaController {
 
     @GetMapping
     public List<Mpa> getMpaRatings() {
+        log.info("Запрос на получение всех рейтингов MPA.");
         return mpaService.getAll();
     }
 
     @GetMapping("/{id}")
     public Mpa getMpa(@PathVariable int id) {
-        return mpaService.showById(id);
+        log.info("Запрос на получение рейтинга MPA с id = {}.", id);
+        Mpa mpa = mpaService.showById(id);
+
+        if (mpa == null) {
+            throw new IncorrectParamException("Рейтинг MPA с id " + id + " не найден.");
+        }
+
+        return mpa;
     }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ResourceNotFoundException.java
@@ -0,0 +1,6 @@
+package ru.yandex.practicum.filmorate.controller;
+
+public class ResourceNotFoundException extends Throwable {
+    public ResourceNotFoundException(Object s) {
+    }
+}
