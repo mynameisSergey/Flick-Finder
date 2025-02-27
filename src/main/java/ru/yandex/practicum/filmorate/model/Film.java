b70612a3063b9894d3e2487a739ceb66d2c82580
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-02-27 22:58:17 +0300

    fix: переопределены методы equals and hashCode

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.model;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +8,7 @@ import lombok.NoArgsConstructor;
 import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Data
@@ -17,20 +17,28 @@ import java.util.Set;
 @Builder
 public class Film {
     private int id;
-    @NotNull(message = "Film name cannot be not null")
-    @NotEmpty(message = "Film name cannot be not empty")
-    @NotBlank(message = "Film name cannot be not blank")
+
+    @NotNull(message = "Film name cannot be null")
+    @NotEmpty(message = "Film name cannot be empty")
+    @NotBlank(message = "Film name cannot be blank")
     private String name;
-    @NotEmpty
+
+    @NotEmpty(message = "Film description cannot be empty")
     @Size(max = 200, message = "Film description must be less than 200 characters.")
     private String description;
+
+    @PastOrPresent(message = "Release date must be in the past or present")
     private LocalDate releaseDate;
-    @Min(value = 0L, message = "Film duration must be more that 0")
+
+    @Min(value = 1L, message = "Film duration must be greater than 0")
     private long duration;
-    @NotNull(message = "Film need to be rated")
+
+    @NotNull(message = "Film needs to be rated")
     private Mpa mpa;
+
     private Set<Genre> genres = new HashSet<>();
 
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
@@ -40,7 +48,6 @@ public class Film {
 
     @Override
     public int hashCode() {
-        return id;
+        return Objects.hash(id);
     }
-
 }
